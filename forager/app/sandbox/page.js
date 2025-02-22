ddfc49a9abf673ebb350e42e855a99ad6266bb1a
uta-jeh4448 <123279355+jeh4448@users.noreply.github.com>
2025-02-22 17:16:11 +0000

    sandbox works now, verifying the pill list now

--- forager/app/sandbox/page.js
+++ forager/app/sandbox/page.js
@@ -1,11 +1,12 @@
 //import Message from "../../components/Message";
 import Pill from "@/components/Pill";
-
+import PillList from "@/components/PillList";
+// import "../../components/PillList"
 export default function SandboxPage() {
     return (
       <div className="page flex justify-center items-center flex-row">
-        <Pill/>
+        <PillList/>
       </div>
     );
 }
-  
+

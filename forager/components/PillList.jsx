89e32852a4685c9e596db22c46c033980e192b4f
uta-jeh4448 <123279355+jeh4448@users.noreply.github.com>
2025-02-24 15:14:07 +0000

    got all pills to show, and titles to be closer to right style

--- forager/components/PillList.jsx
+++ forager/components/PillList.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import Pill from "./Pill";
 import { pills } from "@/data/development";
+import "../styles/globals.css"
+
 
 export default function PillList () {
 
@@ -8,22 +10,16 @@ export default function PillList () {
     const filtersRegions = pills.filter(pill => pill.pillFilterType === 'regions')
     const filtersCategories = pills.filter(pill => pill.pillFilterType === 'categories')
     //const [selectedPill, setSelectedPill] = useState(null);
-
+    
     // const handlePillClick = (filterType) => {
     //     setSelectedPill(filterType);
     // };
     return (
-        
+        <div style={{ backgroundColor: 'white' }}>
         <ul>
-            <li>
-                <h1>Tags</h1>
-                <h1>Regions</h1>
-                <h1>Categories</h1>
-            </li>
-
-            <div>
-            <h1>Select a Pill</h1>
-            {filtersTags.map((pill, index) => (
+                <h1 className="bold-black-title">Tags</h1>
+                <li>
+                {filtersTags.map((pill, index) => (
                 <Pill
                     key={index}
                     pillText={pill.pillText}
@@ -33,14 +29,37 @@ export default function PillList () {
                     //onClick={() => handlePillClick(pill.filterType)} // Optional if you want to perform an action on click
                 />
             ))}
-        </div>
-            <div>
-            {filtersTags.map((tag, index) => (
-                <div key={index} style={{ backgroundColor: tag.pillColor }}>
-                    {tag.pillText}
-                </div>
+        </li>
+                <h1 className="bold-black-title">Regions</h1>
+                <li>
+                {filtersRegions.map((pill, index) => (
+                <Pill
+                    key={index}
+                    pillText={pill.pillText}
+                    pillFilterType={pill.filterType}
+                    //pillSelected={selectedPill === pill.filterType}
+                    pillColor={pill.color}
+                    //onClick={() => handlePillClick(pill.filterType)} // Optional if you want to perform an action on click
+                />
             ))}
-        </div>
+        </li>
+                <h1 className="bold-black-title">Categories</h1>
+                <li>
+                {filtersCategories.map((pill, index) => (
+                <Pill
+                    key={index}
+                    pillText={pill.pillText}
+                    pillFilterType={pill.filterType}
+                    //pillSelected={selectedPill === pill.filterType}
+                    pillColor={pill.color}
+                    //onClick={() => handlePillClick(pill.filterType)} // Optional if you want to perform an action on click
+                />
+              ))}
+             </li>
+
+            
+           
         </ul>
+        </div>
     );
 }

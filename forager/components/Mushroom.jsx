6b89d4d852510032304fa6a9c15121b3acffed0c
uta-jeh4448 <123279355+jeh4448@users.noreply.github.com>
2025-03-04 20:17:34 +0000

    got a background up, not full on Mushroom.jsx

--- forager/components/Mushroom.jsx
+++ forager/components/Mushroom.jsx
@@ -29,7 +29,7 @@ const MushroomCard = ({ mushroom, card }) => {
     };
 
     return (
-        <div className="mushroom-card">
+        <div className="background">
             <img src={image} alt={name} width="290" height="290" />
             {card ? (
                 <h2 className="text-black">{name}</h2>
@@ -38,7 +38,7 @@ const MushroomCard = ({ mushroom, card }) => {
                     <div className="mushroom-text-container">
                         <div className="text-container">
                             <h2 className="text-black">{name}</h2>
-                            <h3>{scientific_name}</h3>
+                            <h3 className="text-black">{scientific_name}</h3>
                         </div>
                         <div 
                             className="circle cursor-pointer" 
@@ -49,7 +49,7 @@ const MushroomCard = ({ mushroom, card }) => {
                     <div className="fast-facts">
                         <p>Toxic: {features.is_toxic ? 'Yes' : 'No'}</p>
                     </div>
-                    <div className="message brown rounded-xl text-sm w-full font-medium text-white px-4 py-4 mx-4" style={{ backgroundColor: '#8E4A49' }}>
+                    <div className="background message brown rounded-xl text-sm w-full font-medium text-black px-4 py-4 mx-4" style={{ backgroundColor: '#8E4A49' }}>
                         <div className="header flex items-center">
                             <h1 className="uppercase">Fast Facts</h1>
                         </div>
